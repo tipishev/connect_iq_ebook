cc8e8a05efbb9cdb4a1bc7c593b685e8ee4b9b9a
Timofey Tipishev <tipishev@gmail.com>
2019-01-13 16:09:23 +0100

    rename package contents dir

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ def readme():
         return f.read()
 
 
-setup(name='connect-iq-ebook',
-      version='0.1',
-      description='Tools for creating Connect IQ eBooks',
+setup(name='garmin-connect-iq-ebook',
+      version='0.2',
+      description='Tools for creating Garmin Connect IQ eBooks',
       url='https://fascin.us/ebook',
       author='Fascinus',
       author_email='fascinus.team@gmail.com',
       license='GPL3',
-      packages=['connect_iq_ebook'],
+      packages=['ebook'],
       include_package_data=True,
       zip_safe=False)
