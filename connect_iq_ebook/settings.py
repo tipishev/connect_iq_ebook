d3e1bd3e685e7e99fde9a8a53eb8f8bae14c2615
Timofey Tipishev <tipishev@gmail.com>
2019-07-21 18:36:43 +0200

    remove monkeybrains.jar

---
+++ connect_iq_ebook/settings.py
@@ -0,0 +1,8 @@
+JAVA_8_PATH = '/usr/bin/java'
+
+# TODO shave the content of this dir to bare essentials
+CONNECT_IQ_BIN_DIR = (
+    '/home/user/connectiq/connectiq-sdk-lin-3.0.11-2019-4-30-cd45859/bin'
+)
+
+EBOOK_SOURCE_LOCATION = 'connect_iq_ebook/connect_iq/ebook'
